6e9ed082be605fdaf27b7dc260ab5efc5bce1baa
Mike Walters <michael.walters@skillsoft.com>
2023-02-07 20:01:34 +0000

    pruning

--- src/seeders/DatabaseSeeder.ts
+++ src/seeders/DatabaseSeeder.ts
@@ -1,19 +1,29 @@
 import { EntityManager } from '@mikro-orm/core';
 import { Seeder } from '@mikro-orm/seeder';
-import { Course } from '../entities';
-import { TestDataHelper } from './testData.helper';
+import { Course, Customization, Page, Topic } from '../entities';
 
 export class DatabaseSeeder extends Seeder {
 
   async run(em: EntityManager): Promise<void> {
-    const helper = new TestDataHelper(em);
-
     const course1 = new Course();
-    course1.published = helper.createCustomization();
+    course1.published = this.createCustomization(em);
     em.persist(course1)
 
     const course2 = new Course();
-    course2.draft = helper.createCustomization();
+    course2.draft = this.createCustomization(em);
     em.persist(course2)
+  }
+
+  public createCustomization(em: EntityManager) {
+    const page = new Page();
+
+    const topic = new Topic();
+    topic.setPages([page]);
+
+    const customization = new Customization();
+    customization.setTopics([topic]);
+    em.persist(customization);
+
+    return customization;
   }
 }
