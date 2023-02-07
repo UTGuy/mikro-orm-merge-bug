1ff720f87382b397fa8ce6b088e40bbc9dedcc6e
Mike Walters <michael.walters@skillsoft.com>
2023-02-07 20:21:38 +0000

    unit test, does not work

--- src/tests/read-update.test.ts
+++ src/tests/read-update.test.ts
@@ -1,55 +1,42 @@
 import { MikroORM } from '@mikro-orm/postgresql';
-import { Embedded, Entity, PrimaryKey, Embeddable, OneToOne, Property, t, FlushMode } from '@mikro-orm/core';
-import { Course, Customization, Media, Page, Topic } from '../entities';
-import { TestDataHelper } from '../seeders/testData.helper';
-import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
+import { Course, Customization, Topic } from '../entities';
+import config from '../mikro-orm.config';
 
 let orm: MikroORM;
 
 beforeAll(async () => {
-    orm = await MikroORM.init({
-        metadataProvider: TsMorphMetadataProvider,
-        entities: [Course, Customization, Topic, Page, Media],
-        type: 'postgresql',
-        host: process.env.DB_CONNECTION_HOST,
-        port: parseInt(process.env.DB_CONNECTION_PORT!),
-        dbName: process.env.DB_CONNECTION_DBNAME,
-        user: process.env.DB_CONNECTION_USER,
-        password: process.env.DB_CONNECTION_PASSWORD,
-        debug: true,
-        flushMode: FlushMode.COMMIT
-    });
+    orm = await MikroORM.init(config);
     await orm.schema.refreshDatabase();
 });
 
 afterAll(() => orm.close(true));
 
 test('json property hydration', async () => {
-    // let em = orm.em.fork();
-    
-    // const helper = new TestDataHelper(em);
+    let em = orm.em.fork();
 
-    // const course1 = new Course();
-    // course1.published = helper.createCustomization(`customization-1-1`);
-    // em.persist(course1)
+    function createCustomization() {
+        const topic = new Topic();
 
-    // const course2 = new Course();
-    // course2.draft = helper.createCustomization(`customization-1-2`);
-    // em.persist(course2)
+        const customization = new Customization();
+        customization.topics = [topic];
+        em.persist(customization);
 
-    // const course3 = new Course();
-    // course3.published = helper.createCustomization(`customization-1-3`);
-    // em.persist(course3)
+        return customization;
+    }
 
-    // const course4 = new Course();
-    // course4.draft = helper.createCustomization(`customization-1-4`);
-    // em.persist(course4)
+    const course1 = new Course();
+    course1.published = createCustomization();
+    em.persist(course1)
 
-    // await em.flush();
-    // em.clear();
+    const course2 = new Course();
+    course2.draft = createCustomization();
+    em.persist(course2)
 
-    // em = em.fork();
-    // const results = await em.find(Course, {}, { populate: true });
-    
+    await em.flush();
+    em.clear();
+
+    em = em.fork();
+    const results = await em.find(Course, {}, { populate: true });
+
     // expect?? - it not to update when a query happens
 });
