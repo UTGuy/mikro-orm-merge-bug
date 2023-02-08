65417151e70e0ba920c46353f992363cf0ee44f4
Mike Walters <michael.walters@skillsoft.com>
2023-02-08 15:16:22 +0000

    added timeout

--- src/tests/read-update.test.ts
+++ src/tests/read-update.test.ts
@@ -4,13 +4,14 @@ import { Course } from '../entities';
 import config from '../mikro-orm.config';
 
 let orm: MikroORM;
+let timeout = 5 * 60 * 1000;
 
 beforeAll(async () => {
     orm = await MikroORM.init(config);
     const seeder = orm.getSeeder();
     await orm.getSchemaGenerator().refreshDatabase();
     await seeder.seed(DatabaseSeeder);
-});
+}, timeout);
 
 afterAll(() => orm.close(true));
 
@@ -20,4 +21,4 @@ test('json property hydration', async () => {
     const results = await repo.find({}, { populate: true });
     await em.flush();
     // expect?? - it not to update when a query happens
-});
+}, timeout);
